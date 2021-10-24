cc105c72c57e7ffc0246bc555f06c122ad06c2fd
tf3 <11424232+TectonicFury@users.noreply.github.com>
2021-10-24 16:33:01 +0530

    added min, max, floor, ceiling, selection, rank, deleteMin and deleteMax

--- src/Test.c
+++ src/Test.c
@@ -1,28 +1,116 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include "bst.h"
 
+// compare functions
 int less_int_(int a, int b) { return a < b;}
 int more_int_(int a , int b) { return a > b;}
+
 //macros - below bst associates keys of type int to int values
 BST(int, int, less_int, more_int)
+void destruct_int_int_pair(int_int_pair int_pair) {
+  free(int_pair); // basic freeing, no recursive freeing for primitive data members (here ints)
+  printf("freed the pair\n");
+}
 
 int main(int argc, char const *argv[]) {
   int_int_bst bst_root = NULL;
   int j;
-  for (size_t i = 0; i < 400000; i++) {
-    j = rand() % 50000;
+
+  // seeding rand
+  srand(time(NULL));
+
+  for (size_t i = 0; i < 600; i++) {
+    j = rand() % 500;
     // inserting random keys and the values (their squares)
+    // you have to put the root on LHS for the effect to take place
     bst_root = insert_int_int_bst(bst_root, j, j * j);
   }
+
+  // testing inorder traversal
   // below list contains the elements inorder
   List_int_int_pair list = recursive_inorder_int_int_bst(bst_root);
+
+  // size of bst is needed to iterate over the list elements
   int size_bst = size_int_int_bst(bst_root);
   printf("bst size = %d\n", size_bst);
   for (size_t i = 0; i < size_bst; i++) {
     printf("key = %d, value = %d\n", list->entry->key, list->entry->value);
     list = list->next;
   }
+
+  // testing the min bst function
+  int_int_pair min = min_int_int_bst(bst_root);
+  if (min) { // min can be NULL if bst_root is NULL
+    printf("min key = %d, min value = %d\n", min->key, min->value);
+  }
+
+  // testing the max bst function
+  int_int_pair max = max_int_int_bst(bst_root);
+  if (max) { // MAX can be null if bst_root is NULL
+    printf("max key = %d, max value = %d\n", max->key, max->value);
+  }
+
+  // testing find_in_int_int_bst
+  int_int_pair found = find_in_int_int_bst(bst_root, 50);
+  if (found) { // need to check for non null since it returns NULL if key is absent
+    printf("found value = %d\n", found->value);
+  }
+
+  // testing floor helper function (it's a helper since it uses an extra paramter for floor_ptr to initiate)
+  int_int_pair floor_pair_helper = helper_floor_int_int_bst(bst_root, 55, NULL);
+  if (floor_pair_helper) {
+    printf("floor key = %d, floor value = %d\n", floor_pair_helper->key, floor_pair_helper->value);
+  }
+
+  // testing the main floor function
+  int_int_pair floor_pair = floor_int_int_bst(bst_root, 65);
+  if (floor_pair) {
+    printf("floor key = %d, floor value = %d\n", floor_pair->key, floor_pair->value);
+  }
+
+  // testing main ceiling function
+  int_int_pair ceiling_pair = ceiling_int_int_bst(bst_root, 65);
+  if (ceiling_pair) {
+    printf("ceiling key = %d, ceiling value = %d\n", ceiling_pair->key, ceiling_pair->value);
+  }
+
+  // testing select_rank_
+  int_int_pair pair_with_rank_10 = select_rank_int_int_bst(bst_root, 10);
+  if (pair_with_rank_10) {
+    printf("key = %d, value = %d\n", pair_with_rank_10->key, pair_with_rank_10->value);
+  }
+
+  // testing rank
+  int rank_of_15 = rank_int_int_bst(bst_root, 15);
+  printf("rank of 15 = %d\n", rank_of_15);
+
+  // testing delete Min
+  if (min) { // printing the previous min value
+    printf("min key = %d, min value = %d\n", min->key, min->value);
+  }
+  printf("old size = %d\n", size_int_int_bst(bst_root));
+  // deleting min key
+  bst_root = deleteMin_int_int_bst(bst_root, destruct_int_int_pair);
+  printf("new size = %d\n", size_int_int_bst(bst_root));
+  min = min_int_int_bst(bst_root); // recalculating min
+  if (min) { // printing the new minimum value
+    printf("min key = %d, min value = %d\n", min->key, min->value);
+  }
+
+  // testing delete max
+  if (max) { // MAX can be null if bst_root is NULL
+    printf("max key = %d, max value = %d\n", max->key, max->value);
+  }
+  bst_root = deleteMax_int_int_bst(bst_root, destruct_int_int_pair);
+  printf("new size = %d\n", size_int_int_bst(bst_root));
+  max = max_int_int_bst(bst_root); // recalculating max
+  if (max) { // MAX can be null if bst_root is NULL
+    printf("max key = %d, max value = %d\n", max->key, max->value);
+  }
+  
+
   return 0;
 }
--- src/bst.h
+++ src/bst.h
@@ -74,6 +74,10 @@ KEY ## _ ## VALUE ## _bst make ## _ ## KEY ## _ ## VALUE ## _bst(KEY key, VALUE 
   p_bst->size = size; \
   return p_bst; \
 } \
+void free_ ## KEY ## _ ## VALUE ## _bst(KEY ## _ ## VALUE ## _bst x) { \
+  /* if the key and value are malloc'ed, we have to free them first before calling this function */ \
+  free(x); \
+} \
 int size_ ## KEY ## _ ## VALUE ## _bst(KEY ## _ ## VALUE ## _bst x) { \
   /* size of node */ \
   if (!x) return 0; \
@@ -92,10 +96,10 @@ KEY ## _ ## VALUE ## _bst insert_ ## KEY ## _ ## VALUE ## _bst(KEY ## _ ## VALUE
                size_ ## KEY ## _ ## VALUE ## _bst(root->right) + 1; \
   return root; \
 } \
-KEY ## _ ## VALUE ## _pair find_in_ ## KEY ## _ ## VALUE ## _bst(KEY ## _ ## VALUE ## _bst root, KEY key, VALUE value) { \
+KEY ## _ ## VALUE ## _pair find_in_ ## KEY ## _ ## VALUE ## _bst(KEY ## _ ## VALUE ## _bst root, KEY key) { \
  if (!root) return NULL; \
- if (IS_LESS ## _(key, root->value_type->key)) return find_in_ ## KEY ## _ ## VALUE ## _bst(root->left, key, value); \
- else if (IS_MORE ## _(key, root->value_type->key)) return find_in_ ## KEY ## _ ## VALUE ## _bst(root->right, key, value); \
+ if (IS_LESS ## _(key, root->value_type->key)) return find_in_ ## KEY ## _ ## VALUE ## _bst(root->left, key); \
+ else if (IS_MORE ## _(key, root->value_type->key)) return find_in_ ## KEY ## _ ## VALUE ## _bst(root->right, key); \
  else return root->value_type; \
 } \
 List_ ## KEY ## _ ## VALUE ## _pair recursive_inorder_ ## KEY ## _ ## VALUE ## _bst(KEY ## _ ## VALUE ## _bst root) { \
@@ -105,5 +109,84 @@ List_ ## KEY ## _ ## VALUE ## _pair recursive_inorder_ ## KEY ## _ ## VALUE ## _
   return append_List_ ## KEY ## _ ## VALUE ## _(3, recursive_inorder_ ## KEY ## _ ## VALUE ## _bst(root->left), \
   make_list_ ## KEY ## _ ## VALUE ## _(root->value_type->key, root->value_type->value), \
   recursive_inorder_ ## KEY ## _ ## VALUE ## _bst(root->right));\
-}
+} \
+KEY ## _ ## VALUE ## _pair min_ ## KEY ## _ ## VALUE ## _bst(KEY ## _ ## VALUE ## _bst root) { \
+  /* return minimum key-value pair */ \
+  if (!root) return NULL; \
+  if (!(root->left)) return root->value_type; \
+  return min_ ## KEY ## _ ## VALUE ## _bst(root->left); \
+} \
+KEY ## _ ## VALUE ## _pair max_ ## KEY ## _ ## VALUE ## _bst(KEY ## _ ## VALUE ## _bst root) { \
+  /* return maximum key-value pair */ \
+  if (!root) return NULL; \
+  if (!(root->right)) return root->value_type; \
+  return max_ ## KEY ## _ ## VALUE ## _bst(root->right); \
+} \
+KEY ## _ ## VALUE ## _pair helper_floor_ ## KEY ## _ ## VALUE ## _bst(KEY ## _ ## VALUE ## _bst root, KEY key, KEY ## _ ## VALUE ## _bst floor_ptr) { \
+  if (!root) { \
+    if (floor_ptr) { \
+      return floor_ptr->value_type; \
+    } else { \
+      return NULL; \
+    }} \
+  if (IS_MORE ## _(key, root->value_type->key)) return helper_floor_ ## KEY ## _ ## VALUE ## _bst(root->right, key, root); \
+  if (IS_LESS ## _(key, root->value_type->key)) return helper_floor_ ## KEY ## _ ## VALUE ## _bst(root->left, key, floor_ptr); \
+  return root->value_type; \
+} \
+KEY ## _ ## VALUE ## _pair floor_ ## KEY ## _ ## VALUE ## _bst(KEY ## _ ## VALUE ## _bst root, KEY key) { \
+  return helper_floor_ ## KEY ## _ ## VALUE ## _bst(root, key, NULL); \
+} \
+KEY ## _ ## VALUE ## _pair helper_ceiling_ ## KEY ## _ ## VALUE ## _bst(KEY ## _ ## VALUE ## _bst root, KEY key, KEY ## _ ## VALUE ## _bst ceiling_ptr) { \
+  if (!root) { \
+    if (ceiling_ptr) { \
+      return ceiling_ptr->value_type; \
+    } else { \
+      return NULL; \
+    } \
+  } \
+  if (IS_LESS ## _(key, root->value_type->key)) return helper_ceiling_ ## KEY ## _ ## VALUE ## _bst(root->left, key, root); \
+  if (IS_MORE ## _(key, root->value_type->key)) return helper_ceiling_ ## KEY ## _ ## VALUE ## _bst(root->right, key, ceiling_ptr); \
+  return root->value_type; \
+} \
+KEY ## _ ## VALUE ## _pair ceiling_ ## KEY ## _ ## VALUE ## _bst(KEY ## _ ## VALUE ## _bst root, KEY key) { \
+  return helper_ceiling_ ## KEY ## _ ## VALUE ## _bst(root, key, NULL); \
+} \
+KEY ## _ ## VALUE ## _pair select_rank_ ## KEY ## _ ## VALUE ## _bst(KEY ## _ ## VALUE ## _bst root, int rank) { \
+  if (!root) return NULL; \
+  int t = size_ ## KEY ## _ ## VALUE ## _bst(root->left); \
+  if (rank < t) return select_rank_ ## KEY ## _ ## VALUE ## _bst(root->left, rank); \
+  if (rank == t) return root->value_type; \
+  return select_rank_ ## KEY ## _ ## VALUE ## _bst(root->right, rank - t - 1); \
+} \
+int rank_ ## KEY ## _ ## VALUE ## _bst(KEY ## _ ## VALUE ## _bst root, KEY key) { \
+  if (!root) return 0; \
+  if (IS_LESS ## _(key, root->value_type->key)) return rank_ ## KEY ## _ ## VALUE ## _bst(root->left, key); \
+  if (IS_MORE ## _(key, root->value_type->key)) return 1 + size_ ## KEY ## _ ## VALUE ## _bst(root->left) + rank_ ## KEY ## _ ## VALUE ## _bst(root->right, key); \
+  return size_ ## KEY ## _ ## VALUE ## _bst(root->left); \
+} \
+KEY ## _ ## VALUE ## _bst deleteMin_ ## KEY ## _ ## VALUE ## _bst(KEY ## _ ## VALUE ## _bst root, void (* destruct) (KEY ## _ ## VALUE ## _pair)) { \
+  /* to properly clean up, we have to pass a function pointer to a custom 'clean-up' function for the specific data in value_type */ \
+  if (!root) return NULL; \
+  if (!root->left) { \
+    destruct(root->value_type); \
+    KEY ## _ ## VALUE ## _bst right_child = root->right; \
+    free(root); \
+    return right_child; \
+  }  \
+  root->left = deleteMin_ ## KEY ## _ ## VALUE ## _bst(root->left, destruct); \
+  root->size = size_ ## KEY ## _ ## VALUE ## _bst(root->left) + size_ ## KEY ## _ ## VALUE ## _bst(root->right) + 1; \
+  return root; \
+} \
+KEY ## _ ## VALUE ## _bst deleteMax_ ## KEY ## _ ## VALUE ## _bst(KEY ## _ ## VALUE ## _bst root, void (* destruct) (KEY ## _ ## VALUE ## _pair)) { \
+  if (!root) return NULL; \
+  if (!root->right) { \
+    destruct(root->value_type); \
+    KEY ## _ ## VALUE ## _bst left_child = root->left; \
+    free(root); \
+    return left_child;\
+  } \
+  root->right = deleteMax_ ## KEY ## _ ## VALUE ## _bst(root->right, destruct); \
+  root->size = size_ ## KEY ## _ ## VALUE ## _bst(root->left) + size_ ## KEY ## _ ## VALUE ## _bst(root->right) + 1; \
+  return root; \
+  }
 #endif
