da55d9984e76d320f0769a011041196a0f1cb92d
tf3 <11424232+TectonicFury@users.noreply.github.com>
2021-10-26 21:23:15 +0530

    minor edits

--- src/rbt.h
+++ src/rbt.h
@@ -38,6 +38,7 @@ List_ ## KEY ## _ ## VALUE ## _pair make_list_ ## KEY ## _ ## VALUE ## _(KEY key
 } \
 void shallow_destroy_List_ ## KEY ## _ ## VALUE ## _rbt(List_ ## KEY ## _ ## VALUE ## _pair list) { \
   /* does not destroy the list-node's copy of pair struct */ \
+  /* need to rename this function (remove the _rbt from the end of function name) and add the same to bst.h */ \
   if (!list) return; \
   List_ ## KEY ## _ ## VALUE ## _pair temp_list_node = list->next; \
   /* destruct(list->entry); */ \
